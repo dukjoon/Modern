fc3a5584176d9fe830c0cedc7ebdeb0034206b39
Dukjoon Jung <ever@Dukjoonui-MacBookAir.local>
2022-10-21 22:58:39 +0900

    dom clear

---
+++ Todo/js/todo.js
@@ -0,0 +1,60 @@
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector(".todo-list");
+const likeButton = document.querySelector(".like");
+
+todoInput.addEventListener("keypress", (e) => {
+    if(e.keyCode === 13){
+        generateTodo(todoInput.value)
+        todoInput.value = "";
+    }
+})
+
+const generateTodo = (todo) => {
+    const li = document.createElement("li");
+    const likeSpan = generateLike();
+    const itemSpan = generateItem(todo);
+    const manageSpan = generateManage();
+    li.appendChild(likeSpan);
+    li.appendChild(itemSpan);
+    li.appendChild(manageSpan);
+    todoList.appendChild(li);
+}
+
+const generateLike = () => {
+    const span = document.createElement("span");
+    span.classList.add("todo-like");
+    const icon = document.createElement("i")
+    icon.classList.add("material-icons");
+    icon.classList.add("like");
+    icon.innerText = "favorite_border";
+    span.appendChild(icon);
+    console.log(span);
+    return span;
+}
+
+const generateItem = (todo) => {
+    const span = document.createElement("span");
+    span.classList.add("todo-item")
+    span.innerText = todoInput.value;
+    return span;
+}
+
+const generateManage = () => {
+    const span = document.createElement("span");
+    span.classList.add("todo-manage");
+    const icon1 = document.createElement("i");
+    const icon2 = document.createElement("i");
+    icon1.classList.add("material-icons");
+    icon1.classList.add("check");
+    icon1.innerText = "check";
+    icon2.classList.add("material-icons");
+    icon2.classList.add("clear");
+    icon2.innerText = "clear";
+    span.appendChild(icon1);
+    span.appendChild(icon2);
+    return span;
+}
+
+
+
+
