f765f2a0cac2166de9dff9363cee65261b65e71c
Dukjoon Jung <ever@Dukjoonui-MacBookAir.local>
2022-11-10 23:31:35 +0900

    타이머기능 구현 완료

--- Typing/js/app.js
+++ Typing/js/app.js
@@ -1,26 +1,53 @@
-const SETTING_TIME = 5;
-let words = ["Banana", "Apple", "Monkey", "Car", "korea", "javascript", "document"];
+const SETTING_TIME = 9;
+let words = [];
+// let isReady = false;
 let time;
 let isloading;
 let score = 0;
 let timeInterval;
-
+let isPlaying
 
 time = 3;
 
-
+const url = "https://random-word-api.herokuapp.com/word?number=100";
 //function
+const getWords = () => {
+    axios.get(url).then(res => {
+        words = res.data.filter(word=> {
+            return word.length < 8
+        })
+        // button.innerText = "시작"
+        // button.classList.remove('loading')
+        // isReady = true;
+    }).catch(err => console.log(err));
+}
+
+const init = () => {
+    time = SETTING_TIME;
+    getWords();
+    
+}
+
 const countDown = () => {
     if(time > 0) {
         time--
-        clearInterval(timeInterval);
+    } else {
+        clearInterval(timeInterval)
+        isPlaying = false;
     }
     timeDisplay.innerText = time
 }
 
 const run = () => {
+    // if (isReady === false) {
+    //     return
+    // }
     timeInterval = setInterval(countDown, 1000);
+    wordInput.value = "";
+    score = 0;
+    isPlaying = true;
 }
+
 
 const wordDisplay = document.querySelector(".word-display");
 const wordInput = document.querySelector(".word-input");
@@ -30,8 +57,10 @@ const button = document.querySelector(".button");
 
 
 const checkMatch = () => {
+    if (!isPlaying) return;
     if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
         score++
+        time = SETTING_TIME
         wordInput.value = ""
         const randomIndex = Math.floor(Math.random() * words.length)
         wordDisplay.innerText = words[randomIndex]
@@ -41,3 +70,6 @@ const checkMatch = () => {
 
 //event handle
 wordInput.addEventListener("input", checkMatch)
+
+//getting ready
+init()
